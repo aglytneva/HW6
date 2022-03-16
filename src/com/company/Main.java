8515bb4ffb9b040b51dad8633cc5f12651c7cd71
Анастасия <kvartira-62@yandex.ru>
2022-03-16 17:42:18 +0300

    ДЗ 6 Выполненное дз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Задание 1
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника = " + fullName);
+        // Задание 2
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета " + fullNameUpper);
+        // Задание 3
+        String fullNameForAdmin =fullName.replace(" ",";");
+        System.out.println("Данные ФИО сотрудника для административного отдела - "+ fullNameForAdmin);
+        String fullNameWithoutMistake = fullName.replace("ё", "е");
+        String fullNameWithoutMistake2 = fullNameWithoutMistake.replace("Ё", "Е");
+        System.out.println("Данные ФИО сотрудника - "+ fullNameWithoutMistake2);
+
     }
 }
